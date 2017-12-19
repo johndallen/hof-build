6ac4c39ccbf450dab83490809d98b775edf00914
Leonard Martin <leonard.martin@gmail.com>
2017-12-19 11:38:26 +0000

    Add environment variable loader to `watch` task
    This allows local environment variables to be defined in a `.env` file in the project to be used for local development. This is easier to manage per-project than setting global env vars (which apply to all projects) or defining vars at runtime using the command line (which is cumbersome to repeat).
    
    Additionally, allowing the file to be specified allows for the easy switching between multiple configuration setups.

--- tasks/watch/index.js
+++ tasks/watch/index.js
@@ -5,6 +5,7 @@ const chokidar = require('chokidar');
 const throttle = require('lodash.throttle');
 const uniq = require('lodash.uniq');
 const cp = require('child_process');
+const path = require('path');
 const match = require('minimatch');
 const chalk = require('chalk');
 const extname = require('path').extname;
@@ -12,6 +13,7 @@ const extname = require('path').extname;
 const tasks = require('../');
 
 const run = require('../../lib/run');
+const env = require('../../lib/env');
 
 module.exports = config => {
 
@@ -134,9 +136,29 @@ module.exports = config => {
     });
   }
 
+  function loadenv() {
+    let envfile = '.env';
+    if (typeof config.env === 'string') {
+      envfile = config.env;
+    }
+    envfile = path.resolve(process.cwd(), envfile);
+    return env(envfile, !!config.env)
+      .then(dotenv => {
+        Object.keys(dotenv).forEach(key => {
+          if (process.env[key] && config.verbose) {
+            console.log(`  Overwriting environment variable ${key} with local value`);
+          }
+          process.env[key] = dotenv[key];
+        });
+      });
+  }
+
   return build(config)
     .then(() => {
       return watch();
+    })
+    .then(() => {
+      return loadenv();
     })
     .then(() => {
       return restart();
